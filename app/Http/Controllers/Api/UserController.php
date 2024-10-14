c4d1845e9a3d5071721a5e0cde3af1cf185ce3cb
Juanpi94 <juang.94.ucr@gmail.com>
2024-10-13 19:08:12 -0600

    get workers modify

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -71,7 +71,7 @@ class UserController extends Controller
             ], 404);
         }
 
-        return UserResource::collection($workers);
+        return $workers;
     }
 
     public function getClients()
