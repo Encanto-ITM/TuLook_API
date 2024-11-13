139669ac4961060778485f820788b14bd5083739
Juanpi94 <juang.94.ucr@gmail.com>
2024-11-13 10:15:02 -0600

    feat: some changes add

---
+++ resources/lang/es/password.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Las siguientes líneas de idioma son las líneas predeterminadas que coinciden
+    | con las razones dadas por el administrador de contraseñas al intentar 
+    | actualizar una contraseña, como por ejemplo para un token inválido o una 
+    | nueva contraseña inválida.
+    |
+    */
+
+    'reset' => '¡Tu contraseña ha sido restablecida!',
+    'sent' => '¡Hemos enviado por correo electrónico el enlace para restablecer tu contraseña!',
+    'throttled' => 'Por favor espera antes de intentar de nuevo.',
+    'token' => 'Este token de restablecimiento de contraseña es inválido.',
+    'user' => "No podemos encontrar un usuario con esa dirección de correo electrónico.",
+];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link rel="stylesheet" href="./css/app.css">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link rel="shortcut icon" href="public/TuLook.ico" type="image/x-icon">
     <!-- Link de bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
 </head>
